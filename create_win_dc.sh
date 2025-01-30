1dbebe2b658a0ed55683de38bb23a4c00b8a85cf
hokuda <hisanobu.okuda@gmail.com>
2025-01-30 19:34:54 +0900

    restrict ports

--- create_win_dc.sh
+++ create_win_dc.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+if [ "x$1" == "x" ]; then
+    echo "$0 <playbook.yml>"
+    exit
+fi
+
 settings=$1
 export DATETIME=`date +%Y%m%d%H%M%S`
 
-time ansible-playbook -v -i hosts ./immortal_vpc.yml -e @key.yml -e @$settings;
+time ansible-playbook -vvv -i hosts ./immortal_vpc.yml -e @key.yml -e @$settings;
+#time ansible-playbook -vvv -i localhost, ./immortal_vpc.yml -e @key.yml -e @$settings;
+#time ansible-playbook -vvv ./immortal_vpc.yml -e @key.yml -e @$settings;
 echo $'\a'
 
 time ansible-playbook -vvv -i hosts ./immortal_windows_dc.yml -e @key.yml -e @$settings ; date;
-echo $'\a'
+echo $'\a'; sleep 1
+echo $'\a'; sleep 1
+echo $'\a'; sleep 1
